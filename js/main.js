255f1545075e63add749b28747c28df386655eb2
Buzzbyte <1598588+buzzbyte@users.noreply.github.com>
2018-07-16 00:45:17 -0400

    rewrote function in a module pattern and deferred script

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 // main.js
 // Contains Javascript functions.....
 
-// Easter egg
-var rawr = function() {
-  document.getElementsByClassName('container')[0].style.paddingTop = 0;
-  document.getElementsByClassName('banner')[0].style.display = "none";
-  document.getElementsByClassName('soon')[0].innerHTML = "<img height='200px' src='images/rawrburt.png'>";
-}
+"use strict";
+
+(function() {
+  var soon = document.getElementsByClassName('soon')[0];
+
+  // Easter egg
+  document.getElementById("rawr").onclick = function() {
+      soon.innerHTML = "";
+      document.getElementsByClassName('banner')[0].style.display = "none";
+      document.getElementsByClassName('container')[0].style.paddingTop = 0;
+      var rawrburt = document.createElement("img")
+      rawrburt.src = "images/rawrburt.png";
+      soon.appendChild(rawrburt);
+  }
+})();
